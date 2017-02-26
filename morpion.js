49c4a796e25067703d0765812c80fa10c720bbda
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2017-02-26 15:51:25 +0100

    add computer mode

--- morpion.js
+++ morpion.js
@@ -6,10 +6,11 @@ var Ncolonnes;
 var prochainJoueur = 1;
 var longueur; // longueur minimal pour gagner
 var joueurs;
-var profondeur = 1;// profondeur recherche ordinateur
+var difficulte = 1;// profondeur recherche ordinateur
+var modeCPU = false;
 
 function output(id){
-    xml_res = "";
+    var xml_res = "";
     for(i = 0; i< Nlignes; i++) {
         xml_res += '&lt;ligne&gt;<br />';
         for(j = 0; j< Ncolonnes; j++) {
@@ -19,7 +20,7 @@ function output(id){
     }
     xml.innerHTML = "<pre><code class='html'>" + xml_res + "</code><pre>";
 
-    content = document.getElementById(id).innerHTML;
+    var content = document.getElementById(id).innerHTML;
     content = content.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {   return '&#'+i.charCodeAt(0)+';'; });
     html_output.innerHTML = '<pre><code class="html">' + content + '</code></p  re>';
     hljs.highlightBlock(html_output);
@@ -28,13 +29,13 @@ function output(id){
 }
 
 function createField(formid, id,xmlid, htmlid="html")  {
-    inputs = document.getElementById(formid).getElementsByTagName('input');
+    var inputs = document.getElementById(formid).getElementsByTagName('input');
     Nlignes = inputs[0].value;
     Ncolonnes = inputs[1].value;
-    field = document.getElementById(id);
+    var field = document.getElementById(id);
     tab = new Array(Nlignes);
     field.innerHTML = "";
-    plateau = "";
+    var plateau = "";
     for(i = 0; i< Nlignes; i++) {
         tab[i] = new Array(Ncolonnes);
         plateau += '\n<div class="morpion-row">\n';
@@ -49,96 +50,89 @@ function createField(formid, id,xmlid, htmlid="html")  {
     html_output = document.getElementById(htmlid);
     output(id);
     longueur = Math.min(Ncolonnes, Nlignes, 5);
-    tous_joueurs = new Array("#337ab7", "#ce4844", "#8B008B", "#FF7F50", "#ADFF2F", "#FFDEAD", "#8B4513", "#2F4F4F", "#696969", "#FFF0F5");
-    joueurs = tous_joueurs.slice(0,inputs[2].value)
+    var tous_joueurs = new Array("#337ab7", "#ce4844", "#8B008B", "#FF7F50", "#ADFF2F", "#FFDEAD", "#8B4513", "#2F4F4F", "#696969", "#FFF0F5");
+    var Njoueur = inputs[2].value;
+    if(Njoueur == 1) {
+        modeCPU = true;
+        joueurs = tous_joueurs.slice(0,2);
+    } else {
+        modeCPU = false;
+        joueurs = tous_joueurs.slice(0,Njoueur);
+    }
+    prochainJoueur = 1;
     return plateau;
 }
 
 
 
 function update(i,j, id) {
-    field = document.getElementById(id);
-    btn = document.getElementById(id + '_' + i + '_' + j);
 
+    if (modeCPU && prochainJoueur == 2) {
+        [i,j] = ordinateur();
+    }
     if(tab[i][j] != 0) {
         return;
     }
 
     tab[i][j] = prochainJoueur;
+    var btn = document.getElementById(id + '_' + i + '_' + j);
     btn.style.backgroundColor = joueurs[prochainJoueur-1];
     if (plusLongueRangee(prochainJoueur).length == longueur) {
-        field.innerHTML = "Joueur <span style='color:" + joueurs[prochainJoueur-1] +";'>" + prochainJoueur + "</span> a gagné !";
+        document.getElementById(id).innerHTML = "Joueur <span style='color:" + joueurs[prochainJoueur-1] +";'>" + prochainJoueur + "</span> a gagné !";
     }
     prochainJoueur = (prochainJoueur < joueurs.length) ? prochainJoueur+1 : 1;
     output(id);
+
+    if (modeCPU && prochainJoueur == 2) {
+        update(i,j, id);
+    }
+
 }
 
 
-function checkWin(symbole) {
-    motif = symbole.toString().repeat(longueur);
-
-    // check lignes
-    for(i = 0; i < Nlignes; i++) {
-        ligne = tab[i].join("")
-        if (ligne.indexOf(motif) != -1)
-            return 1;
-    }
-
-
-    // check colonnes
-    for(j = 0; j < Ncolonnes; j++) {
-        colonne = "";
-        for(i = 0; i < Nlignes; i++)
-            colonne += tab[i][j];
-        if (colonne.indexOf(motif) != -1)
-            return 1;
-    }
-
-    // check diagonales gauche/haut -> droite/bas
-    for (var n = longueur - 1; n < Ncolonnes + Nlignes - 1 - longueur; n ++) {
-        var r = n;
-        var c = 0;
-        var diago = '';
-        while (r >= 0 && c < Ncolonnes) {
-            if (r < Nlignes)
-                diago += tab[r][c];
-            r -= 1;
-            c += 1;
-        }
-        if (diago.indexOf(motif) != -1)
-            return 1;
-    }
-
-    // check diagonales gauche/haut <- droite/bas
-    for (var n =  Nlignes - 1; n >= -Ncolonnes+1; n--) {
-        var r = 0;
-        var c = 0;
-        var diago = '';
-        while (r < Nlignes && c < Ncolonnes) {
-            if (r >= 0)
-                diago += tab[r][c];
-            r += 1;
-            c += 1;
-        }
-        if (diago.indexOf(motif) != -1)
-            return 1;
-    }
-
-    return 0;
-}
-
-function ordinateur() {
-    // trouver la rangée la plus longue
+// mode ordinateur
+// TODO recursivite pour chercher en profondeur
+function ordinateur(tableau = null) {
+    if (!tableau) tableau = tab;
+    var ajouer = [];
 
     // pour toutes les cases ou le joueur a joué, on regarde dans les cases à côté s'il risque de gagner
-    for(i = 0; i< Nlignes; i++) {
-        for(j = 0; j< Ncolonnes; j++) {
-            if(tab[i][j] == 1) {
-                ctab = JSON.parse(JSON.stringify(tab));
-                checkPotentialWin(i,j);
-            }
-        }
+    for(i = 0; i< Nlignes; i++)
+        for (j=0;j <Ncolonnes;j++)
+            if(coupPotentiel(tableau, 1, i, j))
+                ajouer = [i, j];
+
+
+
+    // est-ce qu'une case fait gagner l'ordinateur
+    if (ajouer.length == 0)
+        for(i = 0; i< Nlignes; i++)
+            for (j=0;j <Ncolonnes;j++)
+                if(coupPotentiel(tableau, 2, i, j))
+                    ajouer = [i, j];
+
+
+    // est-ce qu'une case augmente la plus longue rangee de l'ordinateur
+    if (ajouer.length == 0) {
+        actuel = plusLongueRangee(1).length;
+        for(i = 0; i< Nlignes; i++)
+            for (j=0;j <Ncolonnes;j++)
+                if(coupPotentiel(tableau, 2, i, j, actuel))
+                    ajouer = [i, j];
     }
+
+    // on prend une case libre au hasard
+    if (ajouer.length == 0)
+        for(i = 0; i< Nlignes; i++)
+            for (j=0; j<Ncolonnes; j++)
+                if(estDansLeTableau([i,j]) && tableau[i][j] == 0)
+                    ajouer = [i,j];
+
+    return ajouer;
+}
+
+function estDansLeTableau(position) {
+    return (position[0] >= 0 && position[0] < Nlignes && position[1] >= 0 && position[1] < Ncolonnes);
 }
 
 // retourne la taille et l'indice de la plus longue chaine continue de [symbole]
@@ -152,13 +146,14 @@ function longueurMotif(string, symbole) {
 }
 
 // retourne un tableau de positions avec la plus longue chaine continue de [symbole] depuis tab
-function plusLongueRangee(symbole) {
-    maxLong = 0;
-    positions = [];
+function plusLongueRangee(symbole, terrain = null) {
+    if (!terrain) terrain = tab;
+    var maxLong = 0;
+    var positions = [];
 
     // check lignes
-    for(i = 0; i < Nlignes; i++) {
-        [max, argmax] = longueurMotif(tab[i].join(""), symbole)
+    for(var i = 0; i < Nlignes; i++) {
+        var [max, argmax] = longueurMotif(terrain[i].join(""), symbole)
         if (max > maxLong) {
             positions = [];
             for(j = argmax;j<argmax+max;j++)
@@ -169,13 +164,13 @@ function plusLongueRangee(symbole) {
 
 
     // check colonnes
-    for(j = 0; j < Ncolonnes; j++) {
-        colonne = "";
-        for(i = 0; i < Nlignes; i++)
-            colonne += tab[i][j];
-        [max, argmax] = longueurMotif(colonne, symbole)
+    for(var j = 0; j < Ncolonnes; j++) {
+        var colonne = "";
+        for(var i = 0; i < Nlignes; i++)
+            colonne += terrain[i][j];
+        var [max, argmax] = longueurMotif(colonne, symbole)
         if (max > maxLong) {
-            positions = [];
+            var positions = [];
             maxLong = max;
             for(i = argmax;i<argmax+max;i++)
                 positions.push([i,j])
@@ -190,13 +185,13 @@ function plusLongueRangee(symbole) {
         var dposition = [];
         while (r >= 0 && c < Ncolonnes) {
             if (r < Nlignes) {
-                diago += tab[r][c];
+                diago += terrain[r][c];
                 dposition.push([r,c]);
             }
             r -= 1;
             c += 1;
         }
-        [max, argmax] = longueurMotif(diago, symbole)
+        var [max, argmax] = longueurMotif(diago, symbole)
         if (max > maxLong) {
             maxLong = max;
             positions = dposition.slice(argmax, argmax+max);
@@ -211,13 +206,13 @@ function plusLongueRangee(symbole) {
         var dposition = [];
         while (r >= 0 && c >= 0) {
             if (r < Ncolonnes) {
-                diago += tab[r][c];
+                diago += terrain[r][c];
                 dposition.push([r,c]);
             }
             r -= 1;
             c -= 1;
         }
-        [max, argmax] = longueurMotif(diago, symbole)
+        var [max, argmax] = longueurMotif(diago, symbole)
         if (max > maxLong) {
             maxLong = max;
             positions = dposition.slice(argmax, argmax+max);
@@ -229,6 +224,19 @@ function plusLongueRangee(symbole) {
 
 
 
-function checkPotentialWin(tab, i,j) {
-
+// retourne vrai si le coup peut nous faire gagner (ou si ca nous donne une chaine plus longue que "actuel")
+function coupPotentiel(tableau, symbole, i,j, actuel = null) {
+    if(!actuel) actuel = longueur -1;
+    if(estDansLeTableau([i,j]) && tableau[i][j] == 0) {
+        var ctab = new Array(Nlignes);
+        for(k = 0; k< Nlignes; k++) {
+            ctab[k] = new Array(Ncolonnes);
+            for(l = 0; l < Ncolonnes; l++) {
+                ctab[k][l] = tab[k][l];
+            }
+        }
+        ctab[i][j] = 1;
+        return (plusLongueRangee(symbole,ctab).length > actuel);
+    }
+    return false;
 }
